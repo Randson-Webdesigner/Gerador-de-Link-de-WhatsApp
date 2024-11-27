89ccb1a3461db12e0c3b87403bcc3f4ddf3ace78
StudioRandson <randson.farias@gmail.com>
2024-11-27 18:10:00 -0300

    Adiciona favicon e altera caminho da imagem de fundo

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.png" type="image/png">
     <title>Gerador de Links de WhatsApp</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;600&display=swap">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
